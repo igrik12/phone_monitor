11d93840dfd74ae4b397dea697b900a70869c593
Igor <igrik12@hotmail.com>
2020-09-15 06:38:38 +0100

    bumped version to 1.0.4
    - Added tap support to display card

--- lib/tabs/dashboard/display_card.dart
+++ lib/tabs/dashboard/display_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:phone_monitor/controllers/dashboard_controller.dart';
+import 'package:phone_monitor/controllers/homeController.dart';
 import 'package:phone_monitor/widgets/custom_card.dart';
 
 class DisplayCard extends GetView<DashboardController> {
@@ -9,78 +10,81 @@ class DisplayCard extends GetView<DashboardController> {
 
   @override
   Widget build(BuildContext context) {
-    return CustomCard(
-      child: Padding(
-        padding: const EdgeInsets.all(18.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: [
-                Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                    child: SvgPicture.asset(
-                      'assets/icons/screen_measure.svg',
-                      height: 35,
-                    ))
-              ],
-            ),
-            SizedBox(
-              width: Get.width * 0.05,
-            ),
-            Flexible(
-              child: Obx(() => Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Screen Height', textScaleFactor: 1.2),
-                          Text(
-                              "${controller.wrapper.value.display.heightPixels} px",
-                              textScaleFactor: 1.2)
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Screen Width', textScaleFactor: 1.2),
-                          Text(
-                              "${controller.wrapper.value.display.widthPixels} px",
-                              textScaleFactor: 1.2)
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Density', textScaleFactor: 1.2),
-                          Text(
-                              "${controller.wrapper.value.display.densityDpi} ppi",
-                              textScaleFactor: 1.2)
-                        ],
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text('Screen Size', textScaleFactor: 1.2),
-                          Text(
-                              "${controller.wrapper.value.display.screenSize} inch",
-                              textScaleFactor: 1.2)
-                        ],
-                      )
-                    ],
-                  )),
-            )
-          ],
+    return GestureDetector(
+      onTap: () => Get.find<HomeController>().goTo(3),
+      child: CustomCard(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                      child: SvgPicture.asset(
+                        'assets/icons/screen_measure.svg',
+                        height: 35,
+                      ))
+                ],
+              ),
+              SizedBox(
+                width: Get.width * 0.05,
+              ),
+              Flexible(
+                child: Obx(() => Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Screen Height', textScaleFactor: 1.2),
+                            Text(
+                                "${controller.wrapper.value.display.heightPixels} px",
+                                textScaleFactor: 1.2)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Screen Width', textScaleFactor: 1.2),
+                            Text(
+                                "${controller.wrapper.value.display.widthPixels} px",
+                                textScaleFactor: 1.2)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Density', textScaleFactor: 1.2),
+                            Text(
+                                "${controller.wrapper.value.display.densityDpi} ppi",
+                                textScaleFactor: 1.2)
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('Screen Size', textScaleFactor: 1.2),
+                            Text(
+                                "${controller.wrapper.value.display.screenSize} inch",
+                                textScaleFactor: 1.2)
+                          ],
+                        )
+                      ],
+                    )),
+              )
+            ],
+          ),
         ),
       ),
     );
